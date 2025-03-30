2f05d1709a98c85150a763c0fa41198c1fc2e7cf
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-03-30 14:54:54 +0200

    fixed some $PATH problems

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     bind \cf 'file_search; commandline -f repaint'
     bind \cs 'home_dir_search; commandline -f repaint'
     bind \cd 'current_dir_search; commandline -f repaint'
     bind \cy 'yy; commandline -f repaint'
     bind \cg 'history_search; commandline -f repaint'
     export EDITOR=nvim
-    set -x PATH (bash -c 'eval "$(/usr/libexec/path_helper)"; echo $PATH' | tr ':' '\n')
-    if not set -q TMUX
-        tmux
-    end
+set -gx PATH /opt/homebrew/bin /opt/homebrew/sbin (bash -c 'eval "$(/usr/libexec/path_helper)"; echo $PATH' | tr ':' '\n')
+
+#    set -x PATH (bash -c 'eval "$(/usr/libexec/path_helper)"; echo $PATH' | tr ':' '\n')
+    # if not set -q TMUX
+        # tmux
+    # end
 end
--- .config/fish/functions/file_search.fish
+++ .config/fish/functions/file_search.fish
@@ -1,4 +1,3 @@
-# searches current directory for files
 function file_search
     set input $(fd --type f --exclude '.git' --exclude '.idea' --exclude '.obsidian' --exclude '.fingerprint' --hidden . | fzf --reverse --border --border-label "search" --info=hidden --prompt="" --pointer="-" --color=light --height=50% --preview="bat --color always --style=plain {}" --bind "tab:down,shift-tab:up")
     if [ -n "$input" ]
