ecc49647ad32025ab5bf8cb3de0a97ca11578f6a
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-04-26 15:02:30 +0200

    added yazi plugin

---
+++ .config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,52 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    -- check the installation instructions at
+    -- https://github.com/folke/snacks.nvim
+    "folke/snacks.nvim",
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>e",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>E",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = true,
+    keymaps = {
+      show_help = "<f1>",
+      open_file_in_vertical_split = "<c-v>",
+      open_file_in_horizontal_split = "<c-x>",
+      grep_in_directory = "<c-s>",
+      change_working_directory = "<c-w>",
+      cycle_open_buffers = false,
+      open_file_in_tab = false,
+      replace_in_directory = false,
+      copy_relative_path_to_selected_files = false,
+      send_to_quickfix_list = false,
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    -- vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
