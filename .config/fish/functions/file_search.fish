47436f8306226210849ca8b5dd3cb091766688c0
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-08-26 14:23:59 +0200

    change some popup options

--- .config/fish/functions/file_search.fish
+++ .config/fish/functions/file_search.fish
@@ -1,7 +1,7 @@
 function file_search
-    tmux popup -E "fish -c 'fd --type f --exclude '.git' --exclude '.idea' --exclude '.obsidian' --exclude '.fingerprint' --hidden . | fzf --ansi --info=hidden --preview=\"bat --color always --style=plain {}\" --bind \"J:down,K:up\" > $TMP_FISH'"
+    tmux popup -E -d $(pwd) "fish -c 'fd --type f --exclude '.git' --exclude '.idea' --exclude '.obsidian' --exclude '.fingerprint' --hidden . | fzf --ansi --info=hidden --preview=\"bat --color always --style=plain {}\" --bind \"J:down,K:up\" > $TMP_FISH'"
     set input $(cat $TMP_FISH)
-    if [ -n "$input" ]
+    if test -n "$input"
         nvim $input
     end
 end
