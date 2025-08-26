faba5e2e1a92d2578daf776ae82f0a1a3822ed0b
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-08-26 14:28:09 +0200

    remove some bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,33 @@
 if status is-interactive
-    bind \cf 'file_search; commandline -f repaint'
-    bind \cd 'current_dir_search; commandline -f repaint'
+    # bind \cf 'file_search; commandline -f repaint'
+    # bind \cd 'current_dir_search; commandline -f repaint'
+    # bind \ct 'todo; commandline -f repaint'
+    # bind \cv 'parrot; commandline -f repaint'
+    # bind \cx 'hacker_news; commandline -f repaint'
+    # bind \ca 'play_music; commandline -f repaint'
     bind \cy 'yy; commandline -f repaint'
     bind \cs 'history_search; commandline -f repaint'
-    bind \cg 'go_git_repo; commandline -f repaint'
-    bind \ct 'go_studium_todo; commandline -f repaint'
+    bind \cg 'git_status; commandline -f repaint'
+    bind \cn 'quicknotes; commandline -f repaint'
+
     export EDITOR=nvim
+
+    # --- mactex --- #
     set -gx PATH /opt/homebrew/bin /opt/homebrew/sbin (bash -c 'eval "$(/usr/libexec/path_helper)"; echo $PATH' | tr ':' '\n')
 
-    # >>> coursier install directory >>>
+    # --- coursier --- #
     set -gx PATH "$PATH:/Users/gg1/Library/Application Support/Coursier/bin"
-    # <<< coursier install directory <<<
+
+    # --- zoxide --- #
+    zoxide init fish | source
 
     set -x PAGER 'col -bx | nvim -c "set laststatus=0 noruler noshowmode" -c "BaleiaColorize" -c "ZenMode" -c "nnoremap q :qa!<CR>" -'
     set -x MANPAGER $PAGER
     set -x SYSTEMD_PAGER $PAGER
+
     set -x TMP_FISH ~/.config/fish/extras/tmp.fish
     set -x BUF_FISH ~/.config/fish/extras/buf.fish
+
 
     if not set -q TMUX
         tmux
