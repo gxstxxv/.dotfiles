00d844a29a93cebdee3f7725db719863478e1bb1
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-08-26 14:28:57 +0200

    remove floating todo plugin

--- .config/nvim/lua/plugins/floatingtodo.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vimichael/floatingtodo.nvim",
-  config = function()
-    require("floatingtodo").setup {
-      target_file = "/Users/gg1/Documents/Notizen/notes.md",
-      border = "single",
-    }
-  end,
-}
